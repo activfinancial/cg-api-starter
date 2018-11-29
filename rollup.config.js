83d71e1eb743b24d9e5d7acc871c9ed1bfd32140
Luke Elliott <luke.elliott@activfinancial.com>
2018-11-29 12:31:30 +0000

    chore: initial commit.

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import nodeResolve from "rollup-plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
+
+const config = {
+    input: "./src/index.ts",
+    output: {
+        file: "./lib/index.js",
+        format: "umd",
+        name: "activ_cg_api_starter",
+        sourcemap: true,
+        globals: {
+            "@activfinancial/cg-api": "activ"
+        }
+    },
+    external: [ "@activfinancial/cg-api" ],
+    plugins: [
+        nodeResolve({
+            browser: true,
+            modulesOnly: true
+        }),
+        typescript()
+    ]
+};
+
+export default function(args) {
+    if (args["config-serve"]) {
+        config.plugins.push(new serve({
+            contentBase: ".",
+            port: 8880
+        }));
+        // NB can't get livereload working...
+        config.plugins.push(new livereload());
+    }
+
+    if (args["config-production"]) {
+        config.plugins.push(new terser());
+    }
+
+    return config;
+};
